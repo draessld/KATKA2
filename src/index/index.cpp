43e5449107c1eb8b4b0868a9952847a30d5170d8
draessld <dominikadraesslerova@gmail.com>
2024-01-09 15:26:23 +0100

    comments

--- src/index/index.cpp
+++ src/index/index.cpp
@@ -1,22 +1,25 @@
 #include "index.h"
 
-Index::Index(std::filesystem::path output_path) {
+Index::Index(std::filesystem::path output_path)
+{
     config.out_path = output_path;
 }
 
-Index::Index(bool rebuild,std::filesystem::path output_path)  : rebuild(rebuild){
+Index::Index(bool rebuild, std::filesystem::path output_path) : rebuild(rebuild)
+{
     config.out_path = output_path;
 }
 
 Index::~Index() {}
 
-double Index::build(std::filesystem::path text_file){
+double Index::build(std::filesystem::path text_file)
+{
     std::cout << "-=-=-=-=-=-   Building index   ...   " << std::endl;
 
     auto base = std::chrono::high_resolution_clock::now();
     auto startTime = base;
 
-    //TODO  replace BWT/SA/LCP for bigBWT implementation on https://gitlab.com/manzai/Big-BWT/
+    // TODO  replace BWT/SA/LCP for bigBWT implementation on https://gitlab.com/manzai/Big-BWT/
 
     //  Build SA & fm-index
     config.out_path.replace_extension(config.index_suffix);
@@ -30,7 +33,7 @@ double Index::build(std::filesystem::path text_file){
         std::cout << "No index " << config.out_path.filename() << " located. Building now ...   ";
         sdsl::construct(fm_index, text_file, 1);
         sdsl::store_to_file(fm_index, config.out_path);
-        std::cout << " ==> DONE in " << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - startTime).count() << "um"<< std::endl;
+        std::cout << " ==> DONE in " << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - startTime).count() << "um" << std::endl;
     }
 
     startTime = std::chrono::high_resolution_clock::now();
@@ -49,7 +52,7 @@ double Index::build(std::filesystem::path text_file){
         std::cout << "No LCP " << config.out_path.filename() << " located. Building LCP now." << std::endl;
         sdsl::construct(lcp, text_file, 1);
         sdsl::store_to_file(lcp, config.out_path);
-        std::cout << " ==> DONE in " << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - startTime).count() << "um"<< std::endl;
+        std::cout << " ==> DONE in " << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - startTime).count() << "um" << std::endl;
     }
     startTime = std::chrono::high_resolution_clock::now();
 
@@ -66,10 +69,10 @@ double Index::build(std::filesystem::path text_file){
         sdsl::construct(tmp_csa, text_file, 1);
         sdsl::util::assign(rmq_sa_max, sdsl::rmq_succinct_sct<false>(&tmp_csa));
         sdsl::store_to_file(rmq_sa_max, config.out_path);
-        std::cout << " ==> DONE in " << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - startTime).count() << "um"<< std::endl;
+        std::cout << " ==> DONE in " << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - startTime).count() << "um" << std::endl;
     }
     startTime = std::chrono::high_resolution_clock::now();
-    
+
     //  build RMinQ SA support
     config.out_path.replace_extension(config.rmq_sa_suffix_min);
     if (rebuild || !sdsl::load_from_file(rmq_sa_min, config.out_path))
@@ -83,7 +86,7 @@ double Index::build(std::filesystem::path text_file){
         sdsl::construct(tmp_csa, text_file, 1);
         sdsl::util::assign(rmq_sa_min, sdsl::rmq_succinct_sct<true>(&tmp_csa));
         sdsl::store_to_file(rmq_sa_min, config.out_path);
-        std::cout << " ==> DONE in " << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - startTime).count() << "um"<< std::endl;
+        std::cout << " ==> DONE in " << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - startTime).count() << "um" << std::endl;
     }
     startTime = std::chrono::high_resolution_clock::now();
 
@@ -100,7 +103,7 @@ double Index::build(std::filesystem::path text_file){
         sdsl::construct(tmp_lcp, text_file, 1);
         sdsl::util::assign(rmq_lcp_min, sdsl::rmq_succinct_sct<true>(&tmp_lcp));
         sdsl::store_to_file(rmq_lcp_min, config.out_path);
-        std::cout << " ==> DONE in " << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - startTime).count() << "um"<< std::endl;
+        std::cout << " ==> DONE in " << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - startTime).count() << "um" << std::endl;
     }
     startTime = std::chrono::high_resolution_clock::now();
 
@@ -117,131 +120,128 @@ double Index::build(std::filesystem::path text_file){
         size_t i = 0;
         B.resize(text_size);
         char c;
-        while (in.get(c)) {
+        while (in.get(c))
+        {
             B[i] = 0;
-            if(c == '$')
+            if (c == '$')
                 B[i++] = 1;
-            else i++;
+            else
+                i++;
         }
         sdsl::store_to_file(B, config.out_path);
-        std::cout << " ==> DONE in " << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - startTime).count() << "um"<< std::endl;
+        std::cout << " ==> DONE in " << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - startTime).count() << "um" << std::endl;
     }
     sdsl::util::assign(rankB, &B);
 
-    double index_size = size_in_mega_bytes(fm_index) + size_in_mega_bytes(rmq_lcp_min)+ size_in_mega_bytes(rmq_sa_max)+ size_in_mega_bytes(rmq_sa_min)+ size_in_mega_bytes(lcp) + size_in_mega_bytes(rankB);
+    double index_size = size_in_mega_bytes(fm_index) + size_in_mega_bytes(rmq_lcp_min) + size_in_mega_bytes(rmq_sa_max) + size_in_mega_bytes(rmq_sa_min) + size_in_mega_bytes(lcp) + size_in_mega_bytes(rankB);
     std::cout << "-=-=-=-=-=-   Building index - DONE, size:  " << index_size << " MiB." << std::endl;
     return std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - base).count();
 }
 
-int Index::update_range(char c, size_t &length, Index::size_type &olb, Index::size_type &orb, Index::size_type &lb, Index::size_type &rb){
-    // std::cout << "current range "<<c<<'(' << olb  << '/'<< orb << ')' << "length: " << length-1<< std::endl;
-    // std::cout << "number of " << c << " is " << fm_index.bwt.rank(olb,c) << std::endl;
-    int previous_c = 0;
-    int next_c = fm_index.size()-1;
+int Index::update_range(char c, size_t &length, Index::size_type &olb, Index::size_type &orb, Index::size_type &lb, Index::size_type &rb)
+{
     int minllcp;
     int minrlcp;
+    int tmp;
 
-    int number_of_c = fm_index.bwt.rank(olb,c);
-    if (number_of_c!=0)
-    previous_c = fm_index.bwt.select(number_of_c,c); 
-    next_c = fm_index.bwt.select(number_of_c+1,c);
+    int number_of_c = fm_index.bwt.rank(olb, c);
+    int previous_c = 0;
+    int next_c = fm_index.size() - 1;
+
+    if (number_of_c != 0)
+        previous_c = fm_index.bwt.select(number_of_c, c);
+    next_c = fm_index.bwt.select(number_of_c + 1, c);
 
     if (previous_c <= 0) //  previous_c doesnt exists
-        minllcp =  0;
+        minllcp = 0;
     else
-        minllcp = rmq_lcp_min(previous_c,olb);
+        minllcp = rmq_lcp_min(previous_c, olb);
 
-
-    if (next_c >= (int)lcp.size()){  //  next_c doesnt exists
+    if (next_c >= (int)lcp.size())
+    { //  next_c doesnt exists
         minrlcp = 0;
-    }else
-        minrlcp = rmq_lcp_min(orb,next_c);
+    }
+    else
+        minrlcp = rmq_lcp_min(orb, next_c);
 
-    // std::cout << "previous/olb "<<c<<'(' << previous_c  << '/'<< olb << ')' << "minlcp: " << minllcp<< std::endl;
-    // std::cout << "orb/next "<<c<<'(' << orb  << '/'<< next_c << ')' << "minlcp: " << minrlcp<< std::endl;
-
-    // std::cout << lcp[minllcp] << ','  << lcp[minrlcp] << ','<<std::max(lcp[minllcp],lcp[minrlcp]) << std::endl;
-    int tmp;
-    length = std::max(lcp[minllcp],lcp[minrlcp]);   //  maximal prefix length
+    length = std::max(lcp[minllcp], lcp[minrlcp]); //  maximal prefix length
 
     if (length == 0)
     {
-        lb  = 0;
-        rb = fm_index.size()-1;
-        // std::cout << "New range setting: (" << lb  << ','<< rb << ')' << "lcp: " << length << std::endl;
-        if (olb == lb && orb == rb){
+        lb = 0;
+        rb = fm_index.size() - 1;
+        if (olb == lb && orb == rb)
+        {
             return 1;
         }
-        return 0 ;
+        return 0;
     }
-    
 
-    //  find PSV in O(n) - TODO index LCP to get better, or can be done somehow using RMQ structures?
-    //  PSV
+    //  find PSV in O(n) - TODO index LCP to get better, or can be done somehow using existing structures?
     lb = lcp[olb];
-    for (tmp = olb; tmp >=0; tmp--){
-        if(lcp[tmp] < length){
+    for (tmp = olb; tmp >= 0; tmp--)
+    {
+        if (lcp[tmp] < length)
+        {
             lb = tmp;
             break;
         }
     }
 
-    //  find NSV in O(n) - TODO index LCP to get better, or can be done somehow using RMQ structures?
-    //  NSV
+    //  find NSV in O(n) - TODO index LCP to get better, or can be done somehow using existing structures?
     rb = lcp[orb];
-    for (tmp = orb; tmp < (int)lcp.size(); tmp++){
-        if(lcp[tmp] < length){
+    for (tmp = orb; tmp < (int)lcp.size(); tmp++)
+    {
+        if (lcp[tmp] < length)
+        {
             rb = tmp;
             break;
         }
-        if (tmp == (int)lcp.size()-1){
-            rb = lcp.size()-1;
+        if (tmp == (int)lcp.size() - 1)
+        {
+            rb = lcp.size() - 1;
             break;
         }
     }
-    
-    // std::cout << "New range setting: (" << lb  << ','<< rb << ')' << "lcp: " << length << std::endl;
+
     if (olb == lb && orb == rb)
     {
         return 1;
     }
     return 0;
-    
 }
 
 double Index::locate(std::string pattern)
 {
     auto base = std::chrono::high_resolution_clock::now();
-    // std::cout << std::endl;
-    // std::cout << "Locating pattern: " << pattern << std::endl;
+
     size_type lb = 0;
-    size_type rb = fm_index.size()-1; 
-    size_type olb,orb; 
+    size_type rb = fm_index.size() - 1;
+    size_type olb, orb;
+
     size_t length = 1;
     bool stop = false;
 
-
-    for (int i = pattern.size() - 1; i >= 0 ; i--)  //  at most 2*pattern.size() iterations
+    for (int i = pattern.size() - 1; i >= 0; i--) //  at most 2*pattern.size() iterations
     {
-        olb = lb; orb = rb;
-        // std::cout << olb <<  "\t"<< orb <<"\t"<< i << std::endl;
-        sdsl::backward_search(fm_index,olb,orb,pattern[i],lb,rb);
-        // std::cout << "subpttr: " <<  pattern.substr(i,length) << " was found on " << lb << ","<<rb << std::endl;
-        if (lb > rb){
-            // std::cout << "MEM2 has been found: " <<  pattern.substr(i+1,length-1) << " in genomes " << rankB(fm_index[rmq_sa_min(olb, orb)]) << ',' << rankB(fm_index[rmq_sa_max(olb, orb)]) << std::endl;
-            if(length > 1)  // do not write down zero-length MEMs 
-                occurences.emplace_back(i+1,length-1,rankB(fm_index[rmq_sa_min(olb, orb)]),rankB(fm_index[rmq_sa_max(olb, orb)]));  // Save MEM position in pattern, length, leftmost occ genome number, rightmost occ genome number
+        olb = lb;
+        orb = rb;
+        sdsl::backward_search(fm_index, olb, orb, pattern[i], lb, rb);
+        if (lb > rb)
+        {
+            if (length > 1)                                                                                                               // do not write down zero-length MEMs
+                occurences.emplace_back(i + 1, length - 1, rankB(fm_index[rmq_sa_min(olb, orb)]), rankB(fm_index[rmq_sa_max(olb, orb)])); // Save MEM position in pattern, length, leftmost occ genome number, rightmost occ genome number
             // std::cout << i+1 <<'\t' << length-1 << '\t' <<rankB(fm_index[rmq_sa_min(olb, orb)]) << '\t' << rankB(fm_index[rmq_sa_max(olb, orb)]) << std::endl;
-            stop = update_range(pattern[i],length,olb,orb,lb,rb); // get wider range with searched character
-            if (stop)   //  check if range was changed or stop
+            stop = update_range(pattern[i], length, olb, orb, lb, rb); // get wider range with searched character
+            if (stop)                                                  //  check if range was changed or stop
                 break;
-            i++;    //  for searching same character one more time with wider range
+            i++; //  for searching same character one more time with wider range
         }
-        if(i==0){
-            // std::cout << "MEM1 has been found: " <<  pattern.substr(i,length) << " in genomes " << rankB(fm_index[rmq_sa_min(lb, rb)]) << ',' << rankB(fm_index[rmq_sa_max(lb, rb)]) << std::endl;
-            if(length != 0)  // do not write down zero-length MEMs 
-            // std::cout << i <<'\t' << length << '\t' <<rankB(fm_index[rmq_sa_min(lb, rb)]) << '\t' << rankB(fm_index[rmq_sa_max(lb, rb)]) << std::endl;
-                occurences.emplace_back(i,length,rankB(fm_index[rmq_sa_min(lb, rb)]),rankB(fm_index[rmq_sa_max(lb, rb)]));  // Save MEM position in pattern, length, leftmost occ genome number, rightmost occ genome number
+        if (i == 0)
+        {
+            if (length != 0)                                                                                                  // do not write down zero-length MEMs
+                                                                                                                              // std::cout << i <<'\t' << length << '\t' <<rankB(fm_index[rmq_sa_min(lb, rb)]) << '\t' << rankB(fm_index[rmq_sa_max(lb, rb)]) << std::endl;
+                occurences.emplace_back(i, length, rankB(fm_index[rmq_sa_min(lb, rb)]), rankB(fm_index[rmq_sa_max(lb, rb)])); // Save MEM position in pattern, length, leftmost occ genome number, rightmost occ genome number
             break;
         }
         length++;
